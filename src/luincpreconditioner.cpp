12edcf7af3f059e6e0553599dd72c5c7a2f18021
Eero Willman <e.willman@ee.ucl.ac.uk>
2012-10-13 16:40:07 +0100

    optimisations in incomplete LU preconditioner

--- src/luincpreconditioner.cpp
+++ src/luincpreconditioner.cpp
@@ -3,43 +3,34 @@
 LUIncPreconditioner::LUIncPreconditioner(const IRCMatrix &A):
     a(A)
 {
-/*!
+    /*!
  * Creates an incomplere LU preconditioner with zero drop tolerance, LU(0).
- * The implementation is based on the IKJ version of the ILU factorisation 
+ * The implementation is based on the IKJ version of the ILU factorisation
  * in Saad's book (ALGORITHM 10.3: General ILU Factorization, IKJ Version)
  */
-  
-  
-  
-    idx n = A.getNumRows();
-    
-    // 
+
+    const idx n = A.getNumRows();
+
     for (idx i = 1 ; i < n ; ++i)
     {
-        // FOR ROW k
-	for (idx k = 0 ; k < i ; ++k)
+        std::vector<IndVal>::const_iterator rowEnd = a.nonZeros[i].end();
+        std::vector <IndVal> :: iterator k_itr = a.nonZeros[i].begin();
+        // k-INDEX LOOP OVER ALL NONZEROS IN ROW i WITH COLUMN INDEX < i
+        // (CONSTRUCTS L-PART OF LU)
+        while (k_itr->ind < i ) // loop a[i,k]
         {
-            real *aik(NULL);
-            if (a.isNonZero(i,k, aik))
+            const idx k = k_itr->ind;
+            k_itr->val /= a.getValue(k,k);
+            // j-LOOP OVER NONZEROS IN ROW i WITH COL INDEX > k
+            auto j_itr = k_itr + 1;
+            while ( j_itr != rowEnd ) // loop a[i,j]
             {
-                // SCALE L BY DIAGONAL
-	        (*aik)/= a.getValue(k,k); // <- TODO : diagonals could be pre-fetched
-					  //           in parallel to avoid repeated //           searches.
-                
-		// MODIFY U. 
-		// TODO, OPTIMISE:
-		// INDEX j IS OVER A SINGLE ROW, AND THE SEARCH FOR a[i,j] COULD
-		// BE AVOIDED BY LOOPING FROM k+1'TH NON-ZERO COLUMN TO TO
-		// END OF NON-ZEROS IN ROW i. 
-		for (idx j = k+1 ; j < n ; ++j)
-                {
-                    real *aij(NULL);
-                    if ( a.isNonZero(i,j, aij) )
-                       (*aij)-= (*aik)*a.getValue(k,j); // COULD NON-ZEROS IN a[k,j] 
-                                                        // BE ITERATED OVER TO AVOID SEARCH ?
-                }// end for j
-            }
-        }// end for k
+                const idx j = j_itr->ind;
+                j_itr->val -= (k_itr->val)*a.getValue(k,j); // COULD THIS SEARCH BE OPTIMISED OUT?
+                j_itr ++;
+            }// end for/while j
+            k_itr ++;
+        }// end for/while k
     }// end for i
 
 
@@ -55,7 +46,7 @@ void LUIncPreconditioner::solveMxb(Vector &x, const Vector &b) const
 
 void LUIncPreconditioner::forwardSubstitution(Vector &x, const Vector &b) const
 {
-/*!
+    /*!
   Performs forward substitution on the lower diagonal part of the incomplete
   LU factorisation (L).
 
@@ -67,59 +58,47 @@ void LUIncPreconditioner::forwardSubstitution(Vector &x, const Vector &b) const
     x[i] = b[i] - sum( L[i,j]*x[j] )
 
 */
-
+    const idx n = x.getLength();
+    std::vector<IndVal>::const_iterator col_itr;
     // FOR EACH ROW
-    for(idx i = 0 ; i < x.getLength() ; ++i)
+    for(idx i = 0 ; i < n ; ++i)
     {
-
         // FORM SUM OVER ALL LOWER DIAGONAL VALUES
-        real sum(0.0); // ADD IMPLICIT UNIT DIAGONAL ENTRY
-
-        std::vector<IndVal>::const_iterator itr1 = a.nonZeros[i].begin();
-        idx col = itr1->ind;
-        while ( col < i )
+        real sum(0.0);
+        col_itr = a.nonZeros[i].begin();
+        while (col_itr->ind < i)
         {
-            sum += x[col] * (itr1->val);
-            itr1++;
-            col = itr1->ind;
+            sum += x[col_itr->ind] * (col_itr->val);
+            ++col_itr;
         }
         x[i] = b[i]-sum;
     }
-
-
-
-
 }
 
 void LUIncPreconditioner::backwardSubstitution(Vector &x, const Vector &b) const
 {
-/*!
+    /*!
     Performs back-substitution of upper diagonal part of the incomplete
     LU factorisation (U).
 
     U is the upper diagonal matrix, stored in the upper diagonal part of 'a'.
   */
 
-    idx n = b.getLength(); // NUMBER OF ROWS
-
+    const idx n = b.getLength(); // NUMBER OF ROWS
+    std::vector<IndVal>::const_reverse_iterator col_itr;
     // FOR EACH ROW, COUNT BACKWARDS
     for (idx i = n-1 ; i < n ; --i)
     {
-        //real sum(0.0);
         x[i] = b[i];
+        col_itr = a.nonZeros[i].rbegin();
 
-        std::vector<IndVal>::const_reverse_iterator itr = a.nonZeros[i].rbegin();
-        idx col = itr->ind;
-        while ( col > i)
+        while ( col_itr->ind > i)
         {
-            x[i]-=itr->val*x[col];
-            itr++;
-            col = itr->ind;
+            x[i]-=col_itr->val*x[col_itr->ind];
+            ++col_itr;
         }
-        x[i] /= itr->val; // FINALLY DIVISION BY DIAGONAL
-
+        x[i] /= col_itr->val; // FINALLY DIVISION BY DIAGONAL
     }
-
 }
 
 
