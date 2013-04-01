f5b7a11190a2f3041e6a25c3a2c16ed0e034d843
Eero Willman <e.willman@ee.ucl.ac.uk>
2013-04-01 16:47:41 +0100

    cleanup. moved spamtrix_blas definitions to .cpp file

--- include/spamtrix_blas.hpp
+++ include/spamtrix_blas.hpp
@@ -1,195 +1,45 @@
 #ifndef SPAMTRIX_BLAS_H
 #define SPAMTRIX_BLAS_H
-#include <assert.h>
-#include <iostream>
-#include <stdlib.h>
-#include <omp.h>
+
 
 #include <spamtrix_setup.hpp>
-#include <spamtrix_ircmatrix.hpp>
-#include <spamtrix_tdmatrix.hpp>
-#include <spamtrix_vector.hpp>
 
 
 namespace SpaMtrix
 {
+class IRCMatrix;
+class TDMatrix;
+class Vector;
 
 // MATRIX VECTOR MULTIPLICATION b = Ax
-inline void multiply(const IRCMatrix& A,
+void multiply(const IRCMatrix& A,
+              const Vector& x,
+              Vector& b);
+// TRIADIAGONAL MATRIX MULTIPLICATION
+void multiply(const TDMatrix& A,
+              const Vector& x,
+              Vector& b);
+// CONVENIENCE , IN-PLACE SPECIAL MULTIPLICATION...
+real multiply_dot(const IRCMatrix& A,
                      const Vector& x,
-                     Vector& b)
-{
-   /*!
-   * Matrix-Vector multiplication Ax=b.
-   * \n
-   * In python wrappers this function is renamed to IRCMatMul.
-   */
+                     Vector& b);
+// RETURNS A POSITIVE COPY OF A VECTOR 
+SpaMtrix::Vector abs(const SpaMtrix::Vector &vin);
 
-#ifdef DEBUG
-    assert(A.getNumCols() == x.getLength() );
-    assert(b.getLength() == x.getLength() );
-#endif
-    // FOR EACH ROW
-    const idx n = A.getNumRows();
-#ifdef USES_OPENMP
-#pragma omp parallel for
-#endif
-    for (idx i = 0 ; i < n ; ++i)
-    {
-        // FOR EACH COLUMN
-        register real r(0);
-        const idx row_start = A.rows[i];
-        const idx row_end   = A.rows[i+1];
-        for (idx j = row_start ; j < row_end ; ++j)
-        {
-            const idx col = A.cvPairs[j].ind;
-            r += A.cvPairs[j].val * x[col];
-        }//j
-        b[i] = r;
-
-    }//
-}
-
-inline real multiply_dot(const IRCMatrix& A,
-                     const Vector& x,
-                     Vector& b)
-{
-    /*!
-   * MATRIX VECTOR MULTIPLICATION Ax=b
-     also returns dot product of x an b. Used in IterativeSolvers
-   */
-
-#ifdef DEBUG
-    assert(A.getNumCols() == x.getLength() );
-    assert(b.getLength() == x.getLength() );
-#endif
-    // FOR EACH ROW
-    const idx n = A.getNumRows();
-
-    real dp(0);
-#ifdef USES_OPENMP
-#pragma omp parallel for reduction(+:dp)
-#endif
-    for (idx i = 0 ; i < n ; ++i)
-    {
-        // FOR EACH COLUMN
-        register real r(0);
-        const idx row_start = A.rows[i];
-        const idx row_end   = A.rows[i+1];
-        for (idx j = row_start ; j < row_end ; ++j)
-        {
-            const idx col = A.cvPairs[j].ind;
-            r += A.cvPairs[j].val * x[col];
-        }//j
-        b[i] = r;
-        dp+= b[i]*x[i];
-    }//
-    return dp;
-}
-
-
-inline void multiply(const TDMatrix& A,
-                     const Vector& x,
-                     Vector& b)
-{
-    /*!
-   * Matrix vector muliplication Ax = b
-   * where A is a tridiagonal matrix \n \n
-   * In python this function is renamed to TDMatMul. 
-   */
-
-#ifdef DEBUG
-    assert(A.size == x.getLength() );
-#endif
-    idx n = b.getLength();
-    for (idx i = 0 ; i < n ; i++)
-    {
-        b[i] = A.diagonal[i]*x[i];
-        if (i > 0 )
-           b[i]   += A.lower[i-1] * x[i-1];
-        
-        if (i < (n-1) )
-           b[i] += A.upper[i] * x[i+1];
-    }
-}
 // ======================================
 // BLAS LEVEL 1 FUNCTIONS
 // ======================================
-
-inline void scale(const real a, Vector& v)
-{
-    /*!
-   *  v*= a
-   */
-    for (idx i = 0 ; i < v.getLength() ; ++i)
-        v[i]*=a;
-}
-
-
-// VECTOR DOT PRODUCT
-inline real dot(const Vector& v1, const Vector& v2)
-{
-    /*!
-   * Calculates dot product between two vectors v1 and v2
-   */
-#ifdef DEBUG
-    assert(v1.getLength() == v2.getLength() );
-#endif
-    register real d(0.0);
-    const idx n = v1.getLength();
-//#ifdef USES_OPENMP
-//#pragma omp for reduction(+:d)
-//#endif
-    for (idx i = 0 ; i < n ; ++i)
-        d+= v1[i] * v2[i];
-
-    return d;
-}
-inline void axpy(const real a, const Vector& x, Vector& y)
-{
-    /*!
-   * y = y + a*x
-   */
-#ifdef USES_OPENMP
-#pragma omp parallel for
-#endif
-    for (idx i = 0 ; i < x.getLength() ; ++i)
-    {
-        y[i]+= a*x[i];
-    }
-}// end void axpy
-
-inline void aypx(const real a, Vector& y , const Vector& x)
-{
-    /*!
-   * y = a*y + x
-   */
-    const idx n = y.getLength();
-#ifdef USES_OPENMP
-#pragma omp parallel for
-#endif
-    for (idx i = 0; i < n ; ++i)
-    {
-        y[i] = a*y[i] + x[i];
-    }
-
-}// end void aypx
-
-
-// VECTOR SCALAR PRODUCT v2 = a*v1
-//void multiply(const Vector& v1, const real a, Vector& v2);
+void scale(const real a, Vector& v);            // v*= a
+real dot(const Vector& v1, const Vector& v2);   // VECTOR DOT PRODUCT
+void axpy(const real a, const Vector& x, Vector& y);  // y+= a*x
+void aypx(const real a, Vector& y , const Vector& x); // y = a*y + x
 
 
 // ESTIMATES ERROR NORM FOR SYSTEM Ax=b
 // RETURNING (b - Ax).(b - Ax)
 real errorNorm2(const IRCMatrix& A, const Vector& x, const Vector& b);
 real errorNorm2(const TDMatrix& A, const Vector& x, const Vector& b);
-
-inline real norm(const Vector& x)
-{
-    return x.getNorm();
-}
-
+real norm(const Vector& x);
 } // end namespace SpaMtrix
 
 #endif
